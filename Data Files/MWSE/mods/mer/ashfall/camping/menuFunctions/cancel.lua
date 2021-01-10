9ef4561f4d1472929b71b7556b06e4fa7eaae4a5
Jeremyhaakma <jchaakma@gmail.com>
2021-01-10 14:43:32 +1300

    1.1.10: Hotfix for 1.1.9

--- Data Files/MWSE/mods/mer/ashfall/camping/menuFunctions/cancel.lua
+++
@@ -1,5 +0,0 @@
-return {
-    text = tes3.findGMST(tes3.gmst.sCancel).value,
-    callback = function() return true end,
-    doesCancel = true,
-}
--- Data Files/MWSE/mods/mer/ashfall/version.lua
+++
@@ -1,6 +0,0 @@
-local versionFile = io.open("Data Files/MWSE/mods/mer/ashfall/version.txt", "r")
-local version = ""
-for line in versionFile:lines() do -- Loops over all the lines in an open text file
-    version = line
-end
-return version
