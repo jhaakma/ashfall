61f9c2e4bfb5453a3609daf61715b9b9ba563f90
JHaakma <JCHaakma@gmail.com>
2021-04-08 17:21:57 +1200

    Update Branches Placement feature
    Rather than a timer on the cell, branches are now re-enabled 2 weeks
    after they were first picked up. That way the position and number of
    branches in a cell is determined only once, thus preventing the
    possiblity of a frequently visited cell building up lots of branches.

--- Data Files/MWSE/mods/mer/ashfall/camping/--branches.lua
+++ Data Files/MWSE/mods/mer/ashfall/camping/--branches.lua
@@ -1,20 +1,179 @@
 local common = require("mer.ashfall.common.common")
-
+--Branch placement configs
+local daysToRefresh = 14
+local SECONDS_TO_REFRESH = daysToRefresh * 24 * 60
+local MIN_BRANCHES_PER_TREE = 0
+local MAX_BRANCHES_PER_TREE = 2
+local MIN_DISTANCE_FROM_TREE = 150
+local MAX_DISTANCE_FROM_TREE = 400
+local delay = 1
+local delayMulti = 0.0005
+local maxSteepness = 0.7
 --[[
     Dynamically adds wooden branches around the base of trees.
 
     - Branches placed randomly on cell change
-    - Restocks per cell every week or so
+    - Picked up branches are re-enabled every couple of weeks
     - Placed with random rotation
     - Branch appearance is randomised
 
 ]]
+local function isBranch(reference)
+    return common.staticConfigs.branchIds[reference.object.id:lower()]
+end
+
+local function isTree(reference)
+    return common.staticConfigs.activatorConfig.list.tree:isActivator(reference.object.id)
+end
+local function formatCellId(cell)
+    return cell.editorName:lower()
+end
+--
+local cell_list
+local ignore_list
+local function addBranchesToTree(tree)
+    local numBranchesToPlace = math.random(MIN_BRANCHES_PER_TREE, MAX_BRANCHES_PER_TREE)
+    for _ = 1, numBranchesToPlace do
+        local newDuration = (delay * delayMulti)
+        timer.start{
+            duration = newDuration,
+            callback = function()
+                --initial position is randomly near the tree, 500 units higher than the tree's origin
+                local position = {
+                    tree.position.x + ( math.random(MIN_DISTANCE_FROM_TREE, MAX_DISTANCE_FROM_TREE) * (math.random() < 0.5 and 1 or -1) ),
+                    tree.position.y + ( math.random(MIN_DISTANCE_FROM_TREE, MAX_DISTANCE_FROM_TREE) * (math.random() < 0.5 and 1 or -1) ),
+                    tree.position.z + 500
+                }
+                --Branches are all of slightly different sizes
+                local scale = math.random(80, 100) * 0.01
+
+                --Select a branch mesh based on region
+                local thisRegion = tes3.getRegion().id
+                local branchGroup = common.staticConfigs.branchRegions[thisRegion] or common.staticConfigs.defaultBranchGroup
+                local choice = table.choice(branchGroup) 
+
+                --Create the branch
+                local branch = tes3.createReference{
+                    object = choice,
+                    position = position,
+                    orientation =  {0, 0, 0},
+                    cell = tree.cell,
+                    scale = scale
+                }
+                table.insert(ignore_list, branch)
+
+                --Drop and orient the branch on the ground
+                local didOrient = common.helper.orientRefToGround({ ref = branch, ignore_list = ignore_list })
+                --Check for fail conditions
+                if not didOrient then 
+                    branch:disable() 
+                    mwscript.setDelete{ reference = branch}
+                end
+                --Too steep means it landed on a wall or something
+                local tooSteep = math.abs(branch.orientation.x) > maxSteepness or math.abs(branch.orientation.y) > maxSteepness
+                if tooSteep then
+                    branch:disable()
+                    mwscript.setDelete{ reference = branch}
+                    return
+                end
+
+                --Add some random orientation
+                branch.orientation = {
+                    branch.orientation.x,
+                    branch.orientation.y,
+                    math.remap(math.random(), 0, 1, -math.pi, math.pi)
+                }
+                --set lastUpdated so it can be re-enabled after some time
+                branch.data.lastUpdated = tes3.getSimulationTimestamp()
+            end,
+        }
+        delay = delay + 1
+        common.log:debug("delay: %s", (delay * delayMulti))
+    end
+    common.log:debug("Done for Tree %s", tree.id)
+end
+
+local function checkAndRestoreBranch(branch)
+    if branch.disabled and branch.data.lastPickedUp then
+        if branch.data.lastPickedUp < tes3.getSimulationTimestamp() - SECONDS_TO_REFRESH then
+            common.log:debug("Re-enabling branch")
+            branch:enable()
+        end
+    end
+end
+
+local function addBranchesToCell(cell)
+
+    --only add branches to cells we haven't added them to before
+    if not cell_list[formatCellId(cell)] then
+        --Find trees and add branches
+        for reference in cell:iterateReferences(tes3.objectType.static) do
+            if isTree(reference) then
+                common.log:debug("Adding branches to %s", reference.object.id)
+                table.insert(ignore_list, reference)
+                addBranchesToTree(reference)
+            end
+        end
+    end
+
+    --Find previously placed branches and reactivate them if necessary
+    for reference in cell:iterateReferences(tes3.objectType.activator) do
+        if isBranch(reference) then
+            checkAndRestoreBranch(reference)
+        end
+    end
+
+    cell_list[formatCellId(cell)] = true
+
+end
+
+local function updateCells()
+
+    if common.data and common.config.getConfig().enableBranchPlacement then
+        --Get ignore List
+        ignore_list = {}
+        for _, cell in ipairs(tes3.getActiveCells()) do
+            for reference in cell:iterateReferences() do
+                if reference.object.objectType ~= tes3.objectType.static then
+                    common.log:trace("Adding %s to ignore list", reference.object.id)
+                    table.insert(ignore_list, reference)
+                end
+            end
+        end
+
+        --Start adding branches
+        delay = 1
+        for _, cell in ipairs(tes3.getActiveCells()) do
+            if not cell.isInterior then
+                addBranchesToCell(cell)
+            else
+                common.log:debug("Cell is an interior")
+            end
+        end
+    end
+end
+
+event.register("cellChanged", updateCells)
+
+local function onLoad()
+    common.data.cellBranchList = common.data.cellBranchList or {}
+    cell_list = common.data.cellBranchList
+    updateCells()
+end
+
+event.register("Ashfall:dataLoaded", onLoad)
 
 
+
+--[[
+    When activating a branchk, pick up a firewood
+
+    Disable the branch but don't delete it: we will enable it again when enough time has passed
+]]
 local function onActivate(e)
-    if common.staticConfigs.branchIds[string.lower(e.target.object.id)] then
-        mwscript.disable{ reference = e.target }
-        mwscript.setDelete{ reference = e.target}
+    if isBranch(e.target) then
+        e.target.data.lastPickedUp = tes3.getSimulationTimestamp()
+        e.target:disable()
 
         tes3.addItem{
             reference = tes3.player,
@@ -27,166 +186,3 @@ local function onActivate(e)
 end
 
 event.register("activate", onActivate)
-
-local daysToRefresh = 14
-local SECONDS_TO_REFRESH = daysToRefresh * 24 * 60
-local MIN_BRANCHES_PER_TREE = 0
-local MAX_BRANCHES_PER_TREE = 2
-local MIN_DISTANCE_FROM_TREE = 150
-local MAX_DISTANCE_FROM_TREE = 400
-local delay = 1
-local delayMulti = 0.0005
-local maxSteepness = 0.7
-
-
-local cellList
-local cellListVar
-
-local function formatCellId(cell)
-    return string.format("%s-%s-%s", cell.id, cell.gridX, cell.gridY)
-end
-
---Check if a given cell requires a refresh
-local function cellNeedsRefresh(cell)
-    local cellId = formatCellId(cell)
-    --check has been refreshed before
-    if cellList[cellId] then
-        --check last updated
-        if cellList[cellId] < tes3.getSimulationTimestamp() - SECONDS_TO_REFRESH then
-            ----mwse.log("enough time has passed")
-            return true
-        else
-            ----mwse.log("not enough time has passed")
-            return false
-        end
-    else
-        ----mwse.log("cell has never been refreshed")
-        return true
-    end
-end
-
-
-local ignoreList
-
-local function addBranchesToTree(tree)
-
-
-    local branchNum = math.random(MIN_BRANCHES_PER_TREE, MAX_BRANCHES_PER_TREE)
-    for i = 1, branchNum do
-        local newDuration = (delay * delayMulti)
-        timer.start{
-            
-            duration = newDuration,
-            callback = function() 
-                local position = {
-                    tree.position.x + ( math.random(MIN_DISTANCE_FROM_TREE, MAX_DISTANCE_FROM_TREE) * (math.random() < 0.5 and 1 or -1) ),
-                    tree.position.y + ( math.random(MIN_DISTANCE_FROM_TREE, MAX_DISTANCE_FROM_TREE) * (math.random() < 0.5 and 1 or -1) ),
-                    tree.position.z + 500
-                }
-                local scale = math.random(80, 100) * 0.01
-
-                --Select a branch mesh based on region
-                local thisRegion = tes3.getRegion().id
-                local branchGroup = common.staticConfigs.branchRegions[thisRegion] or common.staticConfigs.defaultBranchGroup
-                local choice = table.choice(branchGroup) 
-
-                local branch = tes3.createReference{
-                    object = choice,
-                    position = position,
-                    orientation =  {0, 0, 0},
-                    cell = tree.cell,
-                    scale = scale
-                }
-                table.insert(ignoreList, branch)
-
-                local didOrient = common.helper.orientRefToGround({ ref = branch, ignoreList = ignoreList })
-                if not didOrient then 
-                    branch:disable() 
-                    mwscript.setDelete{ reference = branch}
-                end
-                if math.abs(branch.orientation.x) > maxSteepness or math.abs(branch.orientation.y) > maxSteepness then
-                    branch:disable()
-                    mwscript.setDelete{ reference = branch}
-                    return
-                end
-
-                --Add some random orientation
-                branch.orientation = {
-                    branch.orientation.x,
-                    branch.orientation.y,
-                    math.remap(math.random(), 0, 1, -math.pi, math.pi)
-                }
-            end,
-        }
-        delay = delay + 1
-        --mwse.log("delay: %s", (delay * delayMulti))
-    end
-    --mwse.log("Done for Tree %s", tree.id)
-end
-
-local function getIsTree(reference)
-    if common.staticConfigs.activatorConfig.list.tree:isActivator(reference.object.id) then
-        return true
-    end
-end
-
-local function addBranchesToCell(cell)
-
-    for reference in cell:iterateReferences(tes3.objectType.static) do
-        if getIsTree(reference) then
-            table.insert(ignoreList, reference)
-            addBranchesToTree(reference)
-        end
-    end
-
-    cellList[formatCellId(cell)] = tes3.getSimulationTimestamp()
-    cellListVar:set(cellList)
-end
-
-local function updateCells()
-
-    if common.data and common.config.getConfig().enableBranchPlacement then
-        --Get ignore List
-        ignoreList = {}
-        for _, cell in ipairs(tes3.getActiveCells()) do
-            for reference in cell:iterateReferences() do
-                if reference.object.id == "ashfall_firewood" then
-                    reference:disable()
-                end
-                if reference.object.objectType ~= tes3.objectType.static then
-                    table.insert(ignoreList, reference)
-                end
-            end
-        end
-
-        --Start adding branches
-        for _, cell in ipairs(tes3.getActiveCells()) do
-            if not cell.isInterior then
-                if cellNeedsRefresh(cell) then
-                    addBranchesToCell(cell)
-                else
-                    ----mwse.log("%s doesn't need refresh", cell.id)
-                end
-            else
-                ----mwse.log("in interior")
-            end
-        end
-        delay = 1
-        --mwse.log("Function took %s", (os.clock() - tStart ))
-    end
-end
-
-event.register("cellChanged", updateCells)
-
-local function onLoad()
-    
-    cellListVar= mwse.mcm.createPlayerData{
-        id = "cellBranchList",
-        path = "ashfall",
-        defaultSetting = {}
-    }
-    cellList = cellListVar:get()
-    updateCells()
-end
-
-event.register("loaded", onLoad)
