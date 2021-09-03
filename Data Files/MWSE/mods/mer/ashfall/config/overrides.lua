4fcfe99686b4a33bbaf27a4d059cb1ca807104f1
jhaakma <jchaakma@gmail.com>
2021-09-03 19:44:34 +1200

    Increase cost of bellows in overrides

--- Data Files/MWSE/mods/mer/ashfall/config/overrides.lua
+++ Data Files/MWSE/mods/mer/ashfall/config/overrides.lua
@@ -2,66 +2,54 @@ local staticConfigs = require("mer.Ashfall.config.staticConfigs")
 local overrides = {
 
     --food
-    ["ingred_bread_01"] = {
+    ingred_bread_01 = {
         weight = 0.5,
         value = 10,
     },
 
-    ["food_kwama_egg_01"] = {
+    food_kwama_egg_01 = {
         weight = 0.4,
         value = 2,
     },
-    ["food_kwama_egg_02"] = {
+    food_kwama_egg_02 = {
         weight = 1.0,
         value = 3,
     },
-    ["ingred_ash_yam_01"] = {
+    ingred_ash_yam_01 = {
         weight = 0.5,
         value = 3
     },
-    ["ingred_corkbulb_root_01"] = {
+    ingred_corkbulb_root_01 = {
         weight = 0.4,
         value = 2
     },
-    ["ingred_crab_meat_01"] = {
+    ingred_crab_meat_01 = {
         weight = 0.5,
         value = 2
     },
-    ["ingred_hound_meat_01"] = {
+    ingred_hound_meat_01 = {
         weight = 1.0,
         value = 4
     },
-    ["ingred_rat_meat_01"] = {
+    ingred_rat_meat_01 = {
         weight = 1.0,
         value = 2
     },
-    ["ingred_scrib_jerky_01"] = {
+    ingred_scrib_jerky_01 = {
         weight = 0.5,
         value = 10
     },
-    ["ingred_scuttle_01"] = {
+    ingred_scuttle_01 = {
         weight = 0.5,
         value = 10,
     },
-
---[[
-    Vanilla bug causes items to be unequipped on first load if inventory item named have changed
-
-    So we aren't renaming things anymore.
-]]
-
-    -- --cooking equipment
-    -- ["misc_com_bucket_metal"] = {
-    --     --name = "Bucket: Metal",
-    --     value = 15
-    -- },
-    -- ["misc_com_bucket_01"] = {
-    --     --name = "Bucket: Wood",
-    --     value = 15
-    -- },
-    ["misc_com_iron_ladle"] = {
+    misc_com_iron_ladle = {
         weight = 1,
         value = 5
+    },
+
+    misc_de_bellows10 = {
+        value = 20
     }
 }
 
