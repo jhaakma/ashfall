ab27e3fc45971d9cf8fbb58585bfd73378cf3510
jhaakma <jchaakma@gmail.com>
2022-11-13 00:39:06 +1300

    Add more TR plants

--- Data Files/MWSE/mods/mer/ashfall/items/planter/config.lua
+++ Data Files/MWSE/mods/mer/ashfall/items/planter/config.lua
@@ -3,6 +3,9 @@ local planterConfig = {
         ["ingred_bc_hypha_facia"] = true,
         ["ingred_bc_bungler's_bane"] = true,
         ["ingred_bc_coda_flower"] = true,
+        ["t_ingflor_honeylily_02"] = true,
+        ["t_ingflor_thorntailflower_01"] = true,
+        ["t_ingflor_monkshoodrootpulp_01"] = true,
     },
     plantBlacklist = {
         flora_bm_holly_05 = true,
