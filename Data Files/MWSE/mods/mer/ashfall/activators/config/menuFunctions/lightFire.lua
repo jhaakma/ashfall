f8bd1dc0c16c7f937e3a3578c50ca100447e1350
jhaakma <jchaakma@gmail.com>
2023-03-20 00:55:06 +1300

    Add ability to light fires by striking flint against a metal weapon

--- Data Files/MWSE/mods/mer/ashfall/activators/config/menuFunctions/lightFire.lua
+++ Data Files/MWSE/mods/mer/ashfall/activators/config/menuFunctions/lightFire.lua
@@ -1,4 +1,5 @@
 local common = require ("mer.ashfall.common.common")
+local CraftingFramework = include("CraftingFramework")
 local logger = common.createLogger("lightFire")
 local DURATION_COST = 10
 
@@ -30,6 +31,63 @@ local function filterFireStarter(e)
 end
 
 
+
+local function playerHasFlintAndSteel(e)
+    --check inventory for flint
+    local hasFlint = false
+    for _, stack in pairs(tes3.player.object.inventory) do
+        local flint = CraftingFramework.Material.getMaterial("flint")
+        if flint and flint:itemIsMaterial(stack.object.id) then
+            hasFlint = true
+            break
+        end
+    end
+    --Check for any short or long blade (as the "steel")
+    local hasSteel = false
+    local steelWeapon = nil
+    for _, stack in pairs(tes3.player.object.inventory) do
+        if stack.object.objectType == tes3.objectType.weapon then
+            local validWeaponTypes = {
+                [tes3.weaponType.shortBladeOneHand] = true,
+                [tes3.weaponType.longBladeOneHand] = true,
+                [tes3.weaponType.longBladeTwoClose] = true,
+                [tes3.weaponType.axeOneHand] = true,
+                [tes3.weaponType.axeTwoHand] = true,
+            }
+            if validWeaponTypes[stack.object.type] then
+                local invalidIdPatterns = {
+                    "flint", "chitin", "stone", "glass"
+                }
+                local invalid = false
+                for _, pattern in pairs(invalidIdPatterns) do
+                    if stack.object.id:lower():find(pattern) then
+                        invalid = true
+                        break
+                    end
+                end
+                if not invalid then
+                    hasSteel = true
+                    steelWeapon = stack.object
+                    break
+                end
+            end
+            local validSteelMaterials = {
+                metal = true,
+                steel = true,
+                iron = true
+            }
+            for material, _ in pairs(validSteelMaterials) do
+                local material = CraftingFramework.Material.getMaterial(material)
+                if material and material:itemIsMaterial(stack.object.id) then
+                    hasSteel = true
+                    break
+                end
+            end
+        end
+    end
+    return (hasFlint and hasSteel), steelWeapon
+end
+
 local menuConfig = {
     text = "Light Fire",
     showRequirements = function(campfire)
@@ -40,10 +98,20 @@ local menuConfig = {
         )
     end,
     tooltip = function()
-        return common.helper.showHint("You can light the fire by dropping a flint and steel or a light directly onto it.")
+        return common.helper.showHint("You can light the fire by dropping a flint and steel or a torch directly onto it.")
     end,
     callback = function(campfire)
         timer.delayOneFrame(function()
+            local hasFlintAndSteel, weapon = playerHasFlintAndSteel()
+            if hasFlintAndSteel then
+                if weapon then
+                    tes3.messageBox(string.format("You strike a piece of flint against %s", weapon.name))
+                else
+                    tes3.messageBox("You light the fire with a flint and steel.")
+                end
+                event.trigger("Ashfall:fuelConsumer_Alight", { fuelConsumer = campfire, lighterData = nil})
+                return
+            end
             logger:debug("Opening Inventory Select Menu")
             tes3ui.showInventorySelectMenu{
                 title = "Select Firestarter",
