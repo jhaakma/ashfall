48d74fc76958d7bf71404f39dddb1b9acc908732
jhaakma <jchaakma@gmail.com>
2024-12-01 11:54:05 +1300

    Hotfix

--- Data Files/MWSE/mods/mer/ashfall/integrations/chargenScenarios.lua
+++ Data Files/MWSE/mods/mer/ashfall/integrations/chargenScenarios.lua
@@ -1,53 +1,42 @@
 local ChargenScenarios = include("mer.chargenScenarios")
-
+if not ChargenScenarios then return end
 ---@type ChargenScenarios.ItemListInput[]
 local loadouts = {
     {
         name = "Survival Gear",
         description = "A basic set of equipment for surviving in the wilderness.",
         items = {
-            { id = "ashfall_woodaxe" },
-            { id = "ashfall_flintsteel" },
-            {
-                description = "Cooking Pot",
-                ids = {
-                    "ashfall_cooking_pot",
-                    "ashfall_cooking_pot_iron",
-                    "ashfall_cooking_pot_steel",
-                },
-            },
+            { id = "ashfall_woodaxe", noDuplicates = true },
+            { id = "ashfall_flintsteel", noDuplicates = true },
+            { id = "ashfall_cooking_pot", noDuplicates = true },
             {
                 id = "ashfall_waterskin",
+                noDuplicates = true,
                 data = { waterAmount = 50 },
             },
-            {
-                description = "Tent",
-                ids = {
-                    "ashfall_tent_base_m",
-                    "ashfall_tent_ashl_m",
-                    "ashfall_tent_leather_m"
-                }
-            },
+            { id = "ashfall_tent_base_m", noDuplicates = true },
         }
     },
     {
         name = "Tea Set",
         description = "A tea set, for the calmer moments between adventuring.",
         items = {
-            { id = "ashfall_stand_01" },
+            { id = "ashfall_stand_01", noDuplicates = true },
             {
                 description = "Teapot",
                 id = "ashfall_kettle_08",
+                noDuplicates = true,
                 data = {
                     waterAmount = 100,
                     teaProgress = 100,
+                    waterHeat = 100,
                     waterType = "ingred_hackle-lo_leaf_01"
                 }
             },
             {
                 description = "Teacup",
                 id = "ashfall_teacup_01",
-                count = 2
+                count = 2,
             },
         }
     }
